5260649d7aedb3a3b16c54a355796c19db5e10dd
Fredes <ricardofredes@gmail.com>
2025-06-16 18:04:12 -0300

    feat: testa novo fluxo de envio

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-import { main } from "./main.js"
+import { main } from "./main.js";
 import express from "express";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -23,6 +23,22 @@ app.get("/", (_req, res) => {
 app.post("/api", async (req, res) => {
   const response = await main(req.body);
   return res.json(response);
+});
+
+app.post("/api/slack", async (req, res) => {
+  const response = await main(req.body);
+
+  await fetch(req.body.response_url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(response),
+  }).catch((error) => {
+    console.error("Erro ao enviar resposta para o Slack:", error);
+  });
+
+  return res.status(204).json();
 });
 
 export default app;
