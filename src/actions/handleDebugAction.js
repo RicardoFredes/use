128b0e98581b8e1d06db3c7839ddd9fd75153097
Fredes <ricardofredes@gmail.com>
2025-06-27 16:40:55 -0300

    fix: import with .js

--- src/actions/handleDebugAction.js
+++ src/actions/handleDebugAction.js
@@ -1,4 +1,4 @@
-import { privateMessage } from "../utils/message";
+import { privateMessage } from "../utils/message.js";
 
 export function handleDebugAction(data) {
   return privateMessage("debug", JSON.stringify(data, null, 2));
